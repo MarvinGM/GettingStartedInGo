3594eb4b6ec832438c9a6d1cca7b41c0aa753ed0
MarvinMG <marvinmahilum091197@gmail.com>
2020-11-17 13:57:38 +0800

    doing great

--- 002_short-declaration/main.go
+++ 002_short-declaration/main.go
@@ -4,8 +4,19 @@ import "fmt"
 
 func main() {
 
+	//assign numeric value
 	x := 11
-
 	fmt.Println(x)
 
+	//assigning new value
+	x = 99
+	fmt.Println(x)
+
+	//assign value by expression
+	y := 100 + 11
+	fmt.Println(y)
+
+	//assign string value
+	z := "Ast, Kael"
+	fmt.Println(z)
 }
