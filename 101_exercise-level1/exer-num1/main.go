94b07cce7daf3f513bc28cb22df82f0dcccccbfd
MarvinMG <marvinmahilum091197@gmail.com>
2020-11-19 15:50:44 +0800

    doing great

---
+++ 101_exercise-level1/exer-num1/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println("Diplay in 1 line: ", x, y, z)
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
