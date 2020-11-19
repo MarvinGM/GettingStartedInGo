3354a7561789181865dda02d51f7c91bb8bf9480
MarvinMG <marvinmahilum091197@gmail.com>
2020-11-19 16:49:26 +0800

    doing great

---
+++ 101_exercise-level1/exer-num2/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+
+	fmt.Println("Value of x: ", x)
+	fmt.Println("Value of y: ", y)
+	fmt.Println("Value of z: ", z)
+
+	fmt.Println("Its called Zero value")
+
+}
