ef46e917df0a3c31c9e3b32ce10a1f9455bc4977
MarvinMG <marvinmahilum091197@gmail.com>
2020-11-19 16:24:27 +0800

    doing great

--- 101_exercise-level1/exer-num1/main.go
+++ 101_exercise-level1/exer-num1/main.go
@@ -3,12 +3,16 @@ package main
 import "fmt"
 
 func main() {
+
+	//Using the short declaration Assing values
 	x := 42
 	y := "James Bond"
 	z := true
 
+	//display all of the output in a SINGLE Statement
 	fmt.Println("Diplay in 1 line: ", x, y, z)
 
+	//display the output a MULTIPLE State
 	fmt.Println(x)
 	fmt.Println(y)
 	fmt.Println(z)
