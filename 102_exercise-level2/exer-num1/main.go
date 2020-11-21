a30b1353a6886fee7f0f7df6f39da4c036133009
MarvinMG <marvinmahilum091197@gmail.com>
2020-11-21 19:01:13 +0800

    doing great

---
+++ 102_exercise-level2/exer-num1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 42
+	fmt.Printf("Decimal Value of x :\t%d\n", a)
+	fmt.Printf("Binary Value of x:\t%b\n", a)
+	fmt.Printf("Hex Value of x:\t\t%#x\n", a)
+}
